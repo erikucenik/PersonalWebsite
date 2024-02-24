8a24f59816fc568b93cd6baf3ddd31641b5cbdc8
Erik Ucenik <eriklazaroucenik@gmail.com>
2024-02-24 09:26:44 +0100

    Add titles to pages and a favicon (chosen at random).

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import os
 
 from fastapi import FastAPI, Request
-from fastapi.responses import HTMLResponse
+from fastapi.responses import HTMLResponse, FileResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 
@@ -15,20 +15,27 @@ templates = Jinja2Templates(directory="templates")
 articles_filenames = [filename for filename in os.listdir("articles")]
 articles_filepaths = [os.path.join("articles", filename) for filename in os.listdir("articles")]
 
-for (filename, filepath) in zip(articles_filenames, articles_filepaths):
+def get_article_title_subtitle_and_path(filename, filepath):
     with open(filepath, "r") as f:
         content = f.read()
         header = content.split("\n", 4)
-        article_title = header[1].split(":", 1)[1][1:]
-        article_subtitle = header[2].split(":", 1)[1][1:]
+        article_title = header[1].split(":", 1)[1].strip()
+        article_subtitle = header[2].split(":", 1)[1].strip()
         article_path = filename.rsplit(".", 1)[0]
 
-        article_html = md2article_html(filepath)
-        article_html = f'<h1 class="main__title">{article_title}</h1>' + article_html
+        return (article_title, article_subtitle, article_path)
 
-        filename_html = article_path + ".html"
-        with open(f"articles_html/{filename_html}", "w") as fout:
-            fout.write(article_html)
+
+for (filename, filepath) in zip(articles_filenames, articles_filepaths):
+    (title, subtitle, path) = get_article_title_subtitle_and_path(filename, filepath)
+    (article_title, article_subtitle, article_path) = get_article_title_subtitle_and_path(filename, filepath)
+
+    article_html = md2article_html(filepath)
+    article_html = f'<h1 class="main__title">{title}</h1>' + article_html
+
+    filename_html = path + ".html"
+    with open(f"articles_html/{filename_html}", "w") as fout:
+        fout.write(article_html)
 
 @app.get("/", response_class=HTMLResponse)
 async def home(request: Request):
@@ -50,8 +57,14 @@ async def home(request: Request):
 
     return templates.TemplateResponse(request=request, name="index.html", context={ "articles": articles })
 
-@app.get("/{article_pathname}")
-async def article(request: Request, article_pathname):
-    with open(f"articles_html/{article_pathname}.html", "r") as f:
+@app.get("/articles/{article_name}")
+async def article(request: Request, article_name):
+    (title, _, _) = get_article_title_subtitle_and_path(article_name + ".md", "articles/" + article_name + ".md")
+
+    with open(f"articles_html/{article_name}.html", "r") as f:
         content = f.read()
-        return templates.TemplateResponse(request=request, name="article.html", context={ "html_content": content})
+        return templates.TemplateResponse(request=request, name="article.html", context={ "html_content": content, "article_title": title })
+
+@app.get("/favicon.ico", include_in_schema=False)
+async def favicon():
+    return FileResponse("./static/favicon.ico")
